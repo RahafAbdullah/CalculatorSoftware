be97e3a480cd8dcfc99c77e21a9bf09f6f4d3638
Rahaf Abdullah <rahaf.m.abdullah@gmail.com>
2019-03-29 09:46:23 +0300

    Second commit ..

--- js/controller.js
+++ js/controller.js
@@ -2,19 +2,36 @@ class controller {
     constructor(view, model) {
         this.CView = view;
         this.CModel = model;
+        this.para=0;
     }
 
     updateRes() {
-        let x = this.CView.getVal();
-        let y = this.CModel.solve(x);
-        this.CView.setVal(y);
+        if(this.para == 1) {
+            let x = this.CView.getVal();
+            let y = this.CModel.solve(x);
+            this.CView.setVal(y);
+        }
+        else
+        alert("you have to enter a digit not an operator");
     }
 
     dis(x) {
-        this.CView.disval(x);
+        if (this.para==1)
+        {
+            this.CView.disval(x);
+            this.para=0;
+        }
+        else if(!(x =='+' || x =='-' || x=='*' || x=='/' ))
+       {
+           this.CView.disval(x);
+           this.para=1;
+       }
+       else
+           alert("you have to enter a digit not an operator");
     }
 
     clr() {
         this.CView.clrval();
+        this.para=0;
     }
 }
